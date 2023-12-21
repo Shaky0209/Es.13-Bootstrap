89ccf5ff0ae393163219a11fca24cef3ae9a0e63
Shaky0209 <andrea.busini@netmail.it>
2023-12-21 18:28:26 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,380 @@
+const archiveJazz = [
+    {
+        img: "Jazz/FrankSinatra.jpg",
+        title: "My way the best of Frank",
+        art: "Frank Sinatra",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song a1", "song a2", "song a3", "song a4", "song a5"],
+    },
+    {
+        img: "Jazz/MilesDavis.jpg",
+        title: "Kind of Blue",
+        art: "Miles Davis",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song b1", "song b2", "song b3", "song b4", "song b5"],
+    },
+    {
+        img: "Jazz/SonnyRollins.jpg",
+        title: "Saxophone Colossus",
+        art: "Sonny Rollins",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song c1", "song c2", "song c3", "song c4", "song c5"],
+    },
+    {
+        img: "Jazz/LouRawls.jpg",
+        title: "Unmistakably Lou",
+        art: "Lou Rawls",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song d1", "song d2", "song d3", "song d4", "song d5"],
+    },
+    {
+        img: "Jazz/NicolaArigliano.jpg",
+        title: "Go Man!",
+        art: "Nicola Arigliano",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song e1", "song e2", "song e3", "song e4", "song e5"],
+    },
+    {
+        img: "Jazz/DaveBrubeck.jpg",
+        title: "Time Out",
+        art: "Dave Brubeck",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song f1", "song f2", "song f3", "song f4", "song f5"],
+    },
+    {
+        img: "Jazz/AugustoMartelli.jpg",
+        title: "Blue Genoa Sound",
+        art: "Augusto Martelli",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song g1", "song g2", "song g3", "song g4", "song g5"],
+    },
+    {
+        img: "Jazz/LouisArmstrong.jpg",
+        title: "The Very Best",
+        art: "Louis Armstrong",
+        genre: "Jazz",
+        recTime: "90min",
+        songs: ["song h1", "song h2", "song h3", "song h4", "song h5"],
+    },
+];
+
+const archiveRock = [
+    {
+        img: "Rock/ac-dc.jpg",
+        title: "Black Ice",
+        art: "AC/DC",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song a1", "song a2", "song a3", "song a4", "song a5"],
+    },
+    {
+        img: "Rock/GunsNroses.jpg",
+        title: "Use Your Illusion II",
+        art: "Guns N' Roses",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song b1", "song b2", "song b3", "song b4", "song b5"],
+    },
+    {
+        img: "Rock/RollingStones.jpg",
+        title: "Through The Past Darkly",
+        art: "Rolling Stones",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song c1", "song c2", "song c3", "song c4", "song c5"],
+    },
+    {
+        img: "Rock/RedHotChiliPeppers.jpg",
+        title: "Save The Population",
+        art: "Ret Hot Chili Peppers",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song d1", "song d2", "song d3", "song d4", "song d5"],
+    },
+    {
+        img: "Rock/Nirvana.jpg",
+        title: "Nevermind",
+        art: "Nirvana",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song e1", "song e2", "song e3", "song e4", "song e5"],
+    },
+    {
+        img: "Rock/Blink.jpg",
+        title: "Buddha",
+        art: "Blink 182",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song f1", "song f2", "song f3", "song f4", "song f5"],
+    },
+    {
+        img: "Rock/DeepPurple.jpg",
+        title: "Deep Purple in Rock",
+        art: "Deep Purple",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song g1", "song g2", "song g3", "song g4", "song g5"],
+    },
+    {
+        img: "Rock/Cranberries.jpg",
+        title: "To The Faithful Departed",
+        art: "Cranberries",
+        genre: "Rock",
+        recTime: "90min",
+        songs: ["song h1", "song h2", "song h3", "song h4", "song h5"],
+    },
+];
+
+const archive70s = [
+    {
+        img: "70s/SisterSledge.jpg",
+        title: "Live in Concert",
+        art: "Sister Sledge",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song a1", "song a2", "song a3", "song a4", "song a5"],
+    },
+    {
+        img: "70s/BeeGees.jpg",
+        title: "Stain' Alive",
+        art: "Bee Gees",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song b1", "song b2", "song b3", "song b4", "song b5"],
+    },
+    {
+        img: "70s/EarthWindFire.jpg",
+        title: "Te Essential",
+        art: "Earth, Wind & Fire",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song c1", "song c2", "song c3", "song c4", "song c5"],
+    },
+    {
+        img: "70s/JamesBrown.jpg",
+        title: "Think!",
+        art: "James Brown",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song d1", "song d2", "song d3", "song d4", "song d5"],
+    },
+    {
+        img: "70s/AlanSorrenti.jpg",
+        title: "Tu sei l'unica donna per me",
+        art: "Alan Sorrenti",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song e1", "song e2", "song e3", "song e4", "song e5"],
+    },
+    {
+        img: "70s/BoneyM.jpg",
+        title: "Nightflight to Venus",
+        art: "Boney M.",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song f1", "song f2", "song f3", "song f4", "song f5"],
+    },
+    {
+        img: "70s/Sylvester.jpeg",
+        title: "Sylvester & The Hot Band",
+        art: "Sylvester",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song g1", "song g2", "song g3", "song g4", "song g5"],
+    },
+    {
+        img: "70s/ChakaKhan.jpg",
+        title: "This is My Night",
+        art: "Chaka Khan",
+        genre: "70s",
+        recTime: "90min",
+        songs: ["song h1", "song h2", "song h3", "song h4", "song h5"],
+    },
+];
+
+let imgs = document.getElementsByTagName("img");
+let title = document.getElementsByClassName("card-title");
+let specifications = document.getElementsByClassName("list-group");
+let buttons = document.getElementsByClassName("btn");
+let button = document.createElement("button");
+let node = document.getElementsByClassName("content")[0];
+let songs = [];
+let popUp;
+let bold1;
+let bold2;
+let bold3;
+let paragraph;
+let paragraph1;
+let paragraph2;
+let paragraph3;
+let elements = document.getElementsByClassName("elVar");
+
+let rem = () => {
+    
+    for (i = 0; i < elements.length; i++) {
+
+        specifications[i].children[0].innerHTML = "";
+        specifications[i].children[1].innerHTML = "";
+        specifications[i].children[2].innerHTML = "";
+    }
+}
+
+let jazz = () => {
+
+    rem();
+
+    for (let i = 0; i < archiveJazz.length; i++) {
+
+        imgs[i].setAttribute("src", archiveJazz[i].img);
+        title[i].innerText = archiveJazz[i].title;
+
+        bold1 = document.createElement("b");
+        bold1.setAttribute("class", "elVar");
+        bold1.innerText = "Artist: ";
+        bold2 = document.createElement("b");
+        bold2.setAttribute("class", "elVar");
+        bold2.innerText = "Genre: ";
+        bold3 = document.createElement("b");
+        bold3.setAttribute("class", "elVar");
+        bold3.innerText = "Time Records: ";
+        paragraph1 = document.createElement("span");
+        paragraph1.setAttribute("class", "elVar");
+        paragraph2 = document.createElement("span");
+        paragraph2.setAttribute("class", "elVar");
+        paragraph3 = document.createElement("span");
+        paragraph3.setAttribute("class", "elVar");
+        paragraph1.innerText = archiveJazz[i].art;
+        paragraph2.innerText = archiveJazz[i].genre;
+        paragraph3.innerText = archiveJazz[i].recTime;
+        specifications[i].children[0].appendChild(bold1);
+        specifications[i].children[1].appendChild(bold2);
+        specifications[i].children[2].appendChild(bold3);
+        specifications[i].children[0].appendChild(paragraph1);
+        specifications[i].children[1].appendChild(paragraph2);
+        specifications[i].children[2].appendChild(paragraph3);
+
+        buttons[i].value = i;
+		buttons[i].addEventListener('click', () => {
+            details(archiveRock[i])});
+    }
+}
+
+let rock = () => {
+
+    rem();
+
+    for (let i = 0; i < archiveRock.length; i++) {
+
+        imgs[i].setAttribute("src", archiveRock[i].img);
+        title[i].innerText = archiveRock[i].title;
+        bold1 = document.createElement("b");
+        bold1.innerText = "Artist: ";
+        bold1.setAttribute("class", "elVar");
+        bold2 = document.createElement("b");
+        bold2.innerText = "Genre: ";
+        bold2.setAttribute("class", "elVar");
+        bold3 = document.createElement("b");
+        bold3.innerText = "Time Records: ";
+        bold3.setAttribute("class", "elVar");
+        paragraph1 = document.createElement("span");
+        paragraph1.setAttribute("class", "elVar");
+        paragraph2 = document.createElement("span");
+        paragraph2.setAttribute("class", "elVar");
+        paragraph3 = document.createElement("span");
+        paragraph3.setAttribute("class", "elVar");
+        paragraph1.innerText = archiveRock[i].art;
+        paragraph2.innerText = archiveRock[i].genre;
+        paragraph3.innerText = archiveRock[i].recTime;
+        specifications[i].children[0].appendChild(bold1);
+        specifications[i].children[1].appendChild(bold2);
+        specifications[i].children[2].appendChild(bold3);
+        specifications[i].children[0].appendChild(paragraph1);
+        specifications[i].children[1].appendChild(paragraph2);
+        specifications[i].children[2].appendChild(paragraph3);
+
+        buttons[i].value = i;
+		buttons[i].addEventListener('click', () => {
+            details(archiveRock[i])});
+    }
+}
+
+let y70s = () => {
+
+    rem();
+
+    for (let i = 0; i < archiveRock.length; i++) {
+
+        imgs[i].setAttribute("src", archive70s[i].img);
+        title[i].innerText = archive70s[i].title;
+        bold1 = document.createElement("b");
+        bold1.innerText = "Artist: ";
+        bold1.setAttribute("class", "elVar");
+        bold2 = document.createElement("b");
+        bold2.innerText = "Genre: ";
+        bold2.setAttribute("class", "elVar");
+        bold3 = document.createElement("b");
+        bold3.innerText = "Time Records: ";
+        bold3.setAttribute("class", "elVar");
+        paragraph1 = document.createElement("span");
+        paragraph1.setAttribute("class", "elVar");
+        paragraph2 = document.createElement("span");
+        paragraph2.setAttribute("class", "elVar");
+        paragraph3 = document.createElement("span");
+        paragraph3.setAttribute("class", "elVar");
+        paragraph1.innerText = archive70s[i].art;
+        paragraph2.innerText = archive70s[i].genre;
+        paragraph3.innerText = archive70s[i].recTime;
+        specifications[i].children[0].appendChild(bold1);
+        specifications[i].children[1].appendChild(bold2);
+        specifications[i].children[2].appendChild(bold3);
+        specifications[i].children[0].appendChild(paragraph1);
+        specifications[i].children[1].appendChild(paragraph2);
+        specifications[i].children[2].appendChild(paragraph3);
+
+        buttons[i].value = i;
+		buttons[i].addEventListener('click', () => {
+            details(archive70s[i])});
+        
+    }
+}
+
+let details = (album) => {
+
+    if (popUp) {
+        node.removeChild(popUp);
+    }
+
+    popUp = document.createElement("div");
+    popUp.setAttribute("class", "fixed-top");
+
+
+
+    for (let i = 0; i < album.songs.length; i++) {
+
+        paragraph = document.createElement("p");
+        paragraph.setAttribute("class", "details");
+        paragraph.innerText = album.songs[i];
+        popUp.appendChild(paragraph);
+        popUp.appendChild(button);
+        button.innerText = "Close";
+        button.style.borderRadius = "10px";
+        node.appendChild(popUp);
+        button.addEventListener("click", () => { popUp.style.display = "none"; });
+    };
+}
+
+jazz();
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// ---------------------------------------------------------------------------------------------
+// PER MOTIVI DI TEMPO E PER I PROBLEMI RISCONTRATI NON HO CURATO IN PARTICOLAR MODO I DETTAGLI
+// MI SONO DEDICATO IN MODO PARTICOLARE ALLA FUNZIONALITA' DEL PROGETTO.
+// ---------------------------------------------------------------------------------------------
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
